df4ee15a208fe6a34a7381e9c10a52e8400f870f
Richard Zhong <richard20010808@gmail.com>
2024-06-06 00:45:41 -0400

    Add project files.

---
+++ Cpp_Game/game.cpp
@@ -0,0 +1,117 @@
+#define is_down(b) (input->buttons[b].is_down)
+#define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
+#define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
+
+float player_1_p, player_1_dp, player_2_p, player_2_dp;
+float arena_half_size_x = 85, arena_half_size_y = 45;
+float player_half_size_x = 1.5, player_half_size_y = 10;
+
+float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y, ball_half_size = 1;
+
+int player_1_score, player_2_score;
+
+internal void simulate_player(float* p, float* dp, float ddp, float dt) {
+	ddp -= *dp * 10.f;
+
+	*p = *p + *dp * dt + ddp * dt * dt * .5f; // player1 position
+	*dp = *dp + ddp * dt; // player1 velocity
+
+	if (*p + player_half_size_y > arena_half_size_y) {
+		*p = arena_half_size_y - player_half_size_y;
+		*dp *= -1;
+	}
+	else if (*p - player_half_size_y < -arena_half_size_y) {
+		*p = -arena_half_size_y + player_half_size_y;
+		*dp *= -1;
+	}
+}
+
+internal bool aabb_vs_aabb(float p1x, float p1y, float hs1x, float hs1y, float p2x, float p2y, float hs2x, float hs2y) {
+	return (p1x + hs1x > p2x - hs2x &&
+		p1x - hs1x < p2x + hs2x &&
+		p1y + hs1y > p2y - hs2y &&
+		p1y - hs1y < p2y + hs2y);
+}
+
+internal void simulate_game(Input* input, float dt) {
+	clear_screen(0xffffff);
+	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0x000000);
+
+	float player_1_ddp = 0.f; // unit per second instead of frame
+
+	if (is_down(BUTTON_UP)) player_1_ddp += 2000;
+	if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
+
+	float player_2_ddp = 0.f; // unit per second instead of frame
+
+	if (is_down(BUTTON_W)) player_2_ddp += 2000;
+	if (is_down(BUTTON_S)) player_2_ddp -= 2000;
+
+	simulate_player(&player_1_p, &player_1_dp, player_1_ddp, dt);
+	simulate_player(&player_2_p, &player_2_dp, player_2_ddp, dt);
+
+	// Simulate Ball
+	{
+		ball_p_x += ball_dp_x * dt;
+		ball_p_y += ball_dp_y * dt;
+
+		// collision detection
+		if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) {
+			ball_p_x = 80 - player_half_size_x - ball_half_size;
+			ball_dp_x *= -1;
+			// ball_dp_y = player_1_dp * .75f;  ball carries player's velocity
+			ball_dp_y = (ball_p_y - player_1_p) + player_1_dp * .7f;
+		}
+		else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) {
+			ball_p_x = -80 + player_half_size_x + ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_2_p) + player_2_dp * .7f;
+		}
+
+		// ball bounce away from the top and bottom of the arena
+		if (ball_p_y + ball_half_size > arena_half_size_y) {
+			ball_p_y = arena_half_size_y - ball_half_size;
+			ball_dp_y *= -1;
+		}
+		else if (ball_p_y - ball_half_size < -arena_half_size_y) {
+			ball_p_y = -arena_half_size_y + ball_half_size;
+			ball_dp_y *= -1;
+		}
+
+		// scoring
+		if (ball_p_x + ball_half_size > arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+			player_1_score++;
+		}
+		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+			player_2_score++;
+		}
+	}
+	/*
+	float at_x = -80;
+	for (int i = 0; i < player_1_score; i++) {
+		draw_rect(at_x, 47.f, 1.f, 1.f, 0xaaaaaa);
+		at_x += 2.5f;
+	}
+
+	at_x = 80;
+	for (int i = 0; i < player_2_score; i++) {
+		draw_rect(at_x, 47.f, 1.f, 1.f, 0xaaaaaa);
+		at_x -= 2.5f;
+	}
+	*/
+	draw_number(player_1_score, -10, 40, 1.f, 0xbbffbb);
+	draw_number(player_2_score, -10, 40, 1.f, 0xbbffbb);
+
+	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
+
+	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0x00ffff);
+	draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0x00ffff);
+}
---
+++ Cpp_Game/renderer.cpp
@@ -0,0 +1,57 @@
+internal void 
+clear_screen(u32 color) {
+	u32* pixel = (u32*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+internal void 
+draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
+
+	x0 = clamp(0, x0, render_state.width);
+	x1 = clamp(0, x1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
+
+	for (int y = y0; y < y1; y++) {
+		u32* pixel = (u32*)render_state.memory + x0 + y*render_state.width;
+		for (int x = x0; x < x1; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+global_variable float render_scale = 0.01f;
+
+internal void
+draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
+	x *= render_state.height * render_scale;
+	y *= render_state.height * render_scale;
+	half_size_x *= render_state.height * render_scale;
+	half_size_y *= render_state.height * render_scale;
+
+	x += render_state.width / 2.f;
+	y += render_state.height / 2.f;
+
+	int x0 = x - half_size_x;
+	int x1 = x + half_size_x;
+	int y0 = y - half_size_y;
+	int y1 = y + half_size_y;
+
+	draw_rect_in_pixels(x0, y0, x1, y1, color);
+}
+
+internal void draw_number(int number, float x, float y, float size, u32 color) {
+	float half_size = size * .5f;
+	switch (number) {
+	case 0: {
+		draw_rect(x - size, y, half_size, size * 2.5f, color);
+		draw_rect(x + size, y, half_size, size * 2.5f, color);
+		draw_rect(x, y + size * 2.f, half_size, half_size, color);
+		draw_rect(x, y - size * 2.f, half_size, half_size, color);
+	} break;
+	}
+}
